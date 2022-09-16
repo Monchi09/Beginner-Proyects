aff2c0b8b80cebb2753bca5f75b2abd360867295
Monchi09 <moisessalazar627@gmail.com>
2022-09-17 01:20:29 +0200

    Game1

---
+++ HangmanGame/javascript.js
@@ -0,0 +1,48 @@
+String.prototype.replaceAt=function(index, character) { return this.substring(0, index) + character + this.substring(index+character.length); } 
+
+
+//Uso: palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra);
+
+const palabras = ['casa', 'perro', 'gato', 'elefante'];
+
+const palabra = palabras[Math.floor(Math.random()*palabras.length)]
+;
+let palabraConGuiones = palabra.replace(/./g, "_ ")
+let contadorFallos = 0;
+
+document.querySelector('#output').innerHTML = palabraConGuiones;
+document.querySelector('#calcular').addEventListener('click',() =>
+{
+    const letra = document.querySelector('#letra').value;
+    let haFallado = true;
+    for(const i in palabra){ 
+        if(letra == palabra[i]){
+            palabraConGuiones = palabraConGuiones.replaceAt(i*2,
+            letra);
+            haFallado = false;
+        }
+    }
+
+    if(haFallado){
+        contadorFallos++;
+        document.querySelector('#juego').style.
+        backgroundPosition = -(264*contadorFallos) + 'px 0';
+        if(contadorFallos == 4){
+            alert("perdiste el juego:(")
+        }
+
+    }else{
+        if(palabraConGuiones.indexOf('_') <0){
+            document.querySelector('#ganador').style.display = 
+            'flex';
+        }
+    }
+
+    document.querySelector('#output').innerHTML = 
+    palabraConGuiones;
+
+    document.querySelector('#letra').value = ''
+    document.querySelector('#letra').focus();
+
+});
+ 
